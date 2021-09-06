berttejeda/bert.webadapter

commit 9d18349c7b215ba5a638588f8dca1eec0da756a7

Show module caller in debug logging

bertdotwebadapter/__init__.py
@@ -14,7 +14,8 @@ class WebAdapter:
   def __init__(self, **kwargs):
     self.fail_on_errors = kwargs.get('fail_on_errors')
     self.verify_tls = kwargs.get('verify_tls', True)
-    logger.debug('Module imported by', sys._getframe(1).f_globals.get('__name__'))
+    caller = sys._getframe(1).f_globals.get('__name__', 'N/A')
+    logger.debug('Module imported by', caller)
 
   def write_cache_file(self, response_obj, cache_file_path):
 

commit fa7d604b6eecc308903207e8a5389cc0d508fdb4

Fixed bug introduced in 9d18349

bertdotwebadapter/__init__.py
@@ -15,7 +15,7 @@ class WebAdapter:
     self.fail_on_errors = kwargs.get('fail_on_errors')
     self.verify_tls = kwargs.get('verify_tls', True)
     caller = sys._getframe(1).f_globals.get('__name__', 'N/A')
-    logger.debug('Module imported by', caller)
+    logger.debug('Module imported by %s', caller)
 
   def write_cache_file(self, response_obj, cache_file_path):
 